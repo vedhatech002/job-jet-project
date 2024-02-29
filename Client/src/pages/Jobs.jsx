6f909b5da0a540a62479a9f631fb5290a8aaed6a
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-02-29 19:56:55 +0530

    fix: merge conflict

--- Client/src/pages/Jobs.jsx
+++ Client/src/pages/Jobs.jsx
@@ -17,41 +17,20 @@ export const Jobs = () => {
       setIsLoading(false);
     };
     getData();
-  }, [jobsData]);
+  }, []);
 
   const handleFilter = () => {
+    console.log(searchInput);
     const filter = jobsData.filter(
       (job) =>
-        job?.jobTitle.toLowerCase().includes(searchInput.toLowerCase()) ||
-        job?.jobLocation.toLowerCase().includes(searchInput.toLowerCase())
+        job?.jobLocation.toLowerCase().includes(searchInput.toLowerCase()) ||
+        job?.jobTitle.toLowerCase().includes(searchInput.toLowerCase())
     );
 
     setFilterData(filter);
   };
 
   return (
-<<<<<<< HEAD
-    <div
-      className={`px-8 sm:px-20 py-8 sm:py-10 sm:pb-24 bg-gray-100 min-h-screen`}
-    >
-      <div>
-        <h1 className="text-blue-500 font-semibold text-3xl">Search Jobs</h1>
-      </div>
-      <div className="mt-3 sm:space-x-3 space-x-2 space-y-2 relative">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-56 sm:w-80 px-10 py-2 outline-none bg-gray-200 rounded focus:border-2 focus:border-blue-400 focus:bg-white"
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            viewBox="0 0 24 24"
-            className="fill-current absolute top-[17px] left-2 text-gray-400"
-=======
     <div className="pt-20 font-poppins bg-gray-200  lg:px-12 sm:px-8 px-4 ">
       <div className=" w-full  py-4 ">
         <h1 className="text-lg text-blue-500 font-semibold ">Search Jobs</h1>
@@ -81,41 +60,11 @@ export const Jobs = () => {
           <button
             className="bg-blue-500 text-white font-semibold hover:cursor-pointer border-2 hover:border-2 border-blue-500 hover:bg-gray-100 hover:text-blue-500 py-1.5 px-4 rounded transition hover:transition"
             onClick={handleFilter}
->>>>>>> 9c01fad01adf93cb66f9d00e403b98318129830d
           >
             Search
           </button>
         </div>
       </div>
-<<<<<<< HEAD
-
-      {isLoading ? (
-        // Loading spinner
-        <div className="flex justify-center">
-          <img
-            src="/loading.gif"
-            alt="loading"
-            className="opacity-40 w-[500px]"
-          />
-        </div>
-      ) : filteredData.length === 0 ? (
-        // No data found image when there is no data
-        <div className="text-center mt-20">
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-256/free-data-not-found-1965034-1662569.png?f=webp"
-            alt="loading"
-            className="mx-auto opacity-40"
-            />
-            <p className="text-sm font-bold text-gray-500">No Jobs posted yet</p>
-        </div>
-      ) : (
-        <div className="mt-10 grid xl:grid-cols-2 grid-cols-1 items-center gap-y-10 sm:gap-10">
-          {filteredData.map((job, id) => (
-            <JobCard data={job} key={id} />
-          ))}
-        </div>
-      )}
-=======
       <section className="min-h-screen">
         {isLoading ? (
           // Loading spinner
@@ -143,7 +92,6 @@ export const Jobs = () => {
           </div>
         )}
       </section>
->>>>>>> 9c01fad01adf93cb66f9d00e403b98318129830d
     </div>
   );
 };
--- Client/src/pages/PostAJob.jsx
+++ Client/src/pages/PostAJob.jsx
@@ -65,13 +65,8 @@ const PostAJob = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="bg-gray-100 px-5 lg:px-20 pt-5 pb-10 sm:pb-24">
+    <div className="bg-gray-100 px-5 lg:px-20 pt-5 pb-10 sm:pb-24 pt-24">
       <h1 className="text-blue-500 font-semibold text-2xl">Post A Job Here!</h1>
-=======
-    <div className="bg-gray-100 px-5 lg:px-20  pb-10 sm:pb-24 pt-24">
-      <h1 className="text-blue-500 font-semibold text-3xl">Post A Job Here!</h1>
->>>>>>> 9c01fad01adf93cb66f9d00e403b98318129830d
 
       {/* form */}
       <form
